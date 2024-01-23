e717b524aab42e25eb9b70854cdad756d5113c00
Mohid <mohid.sharif@mail.utoronto.ca>
2024-01-23 07:42:31 +0000

    completed simulation, data download, introduction, data, comments, and compiled paper

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,19 +1,44 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates data relating to cause of deaths of homeless indivuduals
+# Purpose: Downloads and saves the data from opendatatoronto
+# Author: Mohid Sharif
+# Date: 1 January 2024
+# Contact: mohid.sharif@mail.utoronto.ca
 # License: MIT
 # Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Any other information needed? 
 
 
 #### Workspace setup ####
+#install.packages("tidyverse")
+#install.packages("janitor")
+
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(294)
+
+simulated_data <-
+  tibble(
+    # create necessary classifications for cause of deaths of homeless
+    "Cause_of_death" = c("Accident", "Sickness/Disease", "Drug Toxicity", "Homicide", "Suicide", "Other")
+    ,
+    # Simulate data from normal distribution for male deaths
+    "Male" = rnorm(
+      n = 6,
+      mean = 25,
+      sd = 10) |> round()
+  ,
+    # Simulate data from normal distribution for female deaths
+    "Female" = rnorm(
+      n = 6,
+      mean = 25,
+      sd = 10) |> round()
+  )
+
+# show data
+simulated_data
 
 
 
